693de1edc40ac9778d8caeb418b492d2dd0fb786
Andras Serfozo <subztep@gmail.com>
2022-06-30 02:26:57 +0100

    reformat

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-import http from "node:http"
+import { createServer } from "node:http"
 
-if (!process.env.SECRET_STRING) {
+const {
+  SERVER_PORT = 7000,
+  SECRET_NAME: Name = "TestName",
+  SECRET_STRING: SecretString,
+} = process.env
+
+if (!SecretString) {
   console.error("Missing SECRET_STRING environment variable")
   process.exit(1)
 }
 
-const port = process.env.SERVER_PORT || 7000
-
-const server = http.createServer(async (req, res) => {
+const server = createServer(async (req, res) => {
   const buffers = []
   for await (const chunk of req) {
     buffers.push(chunk)
   }
+  console.log(`${req.method} ${req.url}`, Buffer.concat(buffers).toString())
 
   const response = {
     ARN: "xxx",
-    Name: process.env?.SECRET_NAME ?? "TestName",
+    Name,
     VersionId: "x",
-    SecretString: process.env.SECRET_STRING,
+    SecretString,
     VersionStages: ["x"],
     CreatedDate: 0,
   }
-
-  console.log(`${req.method} ${req.url}`, Buffer.concat(buffers).toString())
   console.log("RESPONSE", response)
 
   res.writeHead(200, { "Content-Type": "application/json" })
   res.end(JSON.stringify(response))
 })
 
-server.listen(port, () =>
-  console.log(`Secrets Manager is listening on port ${port}`)
+server.listen(SERVER_PORT, () =>
+  console.log(`Secrets Manager is listening on port ${SERVER_PORT}`)
 )
