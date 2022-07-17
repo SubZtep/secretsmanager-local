47fb2b64ae449f09401d8c7937f5ae653a25a45a
Andras Serfozo <subztep@gmail.com>
2022-07-18 00:01:12 +0100

    tweaks + ver

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const server = createServer(async (req, res) => {
   for await (const chunk of req) {
     buffers.push(chunk)
   }
+  console.log(formattedTime())
   console.log(`${req.method} ${req.url}`, Buffer.concat(buffers).toString())
+  console.log("REQUEST HEADERS", req.headers)
 
   const response = {
     ARN: "xxx",
@@ -26,7 +28,10 @@ const server = createServer(async (req, res) => {
     VersionStages: ["x"],
     CreatedDate: 0,
   }
-  console.log("RESPONSE", response)
+  console.log("RESPONSE", {
+    ...response,
+    SecretString: `${SecretString.substring(0, 24)}...`,
+  })
 
   res.writeHead(200, { "Content-Type": "application/json" })
   res.end(JSON.stringify(response))
@@ -35,3 +40,23 @@ const server = createServer(async (req, res) => {
 server.listen(SERVER_PORT, () =>
   console.log(`Secrets Manager is listening on port ${SERVER_PORT}`)
 )
+
+process.on("SIGINT", shutdown("SIGINT"))
+process.on("SIGTERM", shutdown("SIGTERM"))
+
+function shutdown(name) {
+  return () => {
+    console.log(`${name} signal received`)
+    server.close(() => {
+      console.log("Server closed")
+      process.exit()
+    })
+  }
+}
+
+function formattedTime() {
+  const dt = new Date()
+  return new Date(dt.getTime() - dt.getTimezoneOffset() * 60_000)
+    .toISOString()
+    .replace(/(.*)T(.*)\..*/, "$1 $2")
+}
